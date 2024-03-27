e44d3d6c31c0c03856de661db56542cad5441f56
billyoyo <billyoyo@hotmail.co.uk>
2024-03-27 01:48:48 +0000

    add refresh and update commands

---
+++ league_fantasy/management/commands/refreshgames.py
@@ -0,0 +1,21 @@
+from ...scraper.scrape_games import refresh_game_data_for_tournament
+from ...scraper.score_calculator import update_all_player_scores_for_tournament
+from ...models import Tournament
+from django.core.management.base import BaseCommand
+
+def daily_refresh_job():
+  active_tournament = Tournament.objects.filter(active=True).first()
+
+  if active_tournament is None:
+    print("no active tournament")
+  else:
+    refresh_game_data_for_tournament(active_tournament)
+    update_all_player_scores_for_tournament(active_tournament)
+    print("updated game data")
+
+class Command(BaseCommand):
+  help = "Runs APScheduler."
+
+  def handle(self, *args, **options):
+    daily_refresh_job()
+  
