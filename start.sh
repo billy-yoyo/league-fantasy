b5955b38909f622a2c471ba5e0fc49b3e8a39dc4
billyoyo <billyoyo@hotmail.co.uk>
2024-03-27 00:15:22 +0000

    fix gunicorn start

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ sudo systemctl reload nginx
 source .venv/bin/activate
 python manage.py migrate --settings league_fantasy.settings.prod
 python manage.py collectstatic --settings league_fantasy.settings.prod
-gunicorn league_fantasy.wsgi --env DJANGO_SETTINGS_MODULE=settings.prod &
+gunicorn league_fantasy.wsgi --env DJANGO_SETTINGS_MODULE=league_fantasy.settings.prod &
 
