d9a0a03f4b00914205835876d985f1b9ad432521
billyoyo <billyoyo@hotmail.co.uk>
2024-03-27 00:13:58 +0000

    fix settings and start script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cp -r ./infrastructure /etc/nginx
 sudo systemctl reload nginx
 
 source .venv/bin/activate
-python manage.py migrate --settings settings.prod
-python manage.py collectstatic --settings settings.prod
+python manage.py migrate --settings league_fantasy.settings.prod
+python manage.py collectstatic --settings league_fantasy.settings.prod
 gunicorn league_fantasy.wsgi --env DJANGO_SETTINGS_MODULE=settings.prod &
 
